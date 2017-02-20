4add800fba47ce5fdc9147461787d156a3998cde
Romain Blineau <romain.blineau@gmail.com>
2017-02-20 23:16:39 +0100

    create another component + look at shadow DOM

---
+++ first-app/src/app/other/another.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fa-another',
+  template: `
+    <p>
+      another Works!
+    </p>
+  `
+})
+export class AnotherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
