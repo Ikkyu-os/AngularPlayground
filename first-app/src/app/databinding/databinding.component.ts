c1077641948c10780da14fe08e64e06ff2fee3c0
Romain Blineau <romain.blineau@gmail.com>
2017-02-21 22:14:50 +0100

    delete first components + test property binding

---
+++ first-app/src/app/databinding/databinding.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent implements OnInit {
+  stringInterpolation = 'this is a string';
+  numberInterpolation = 2;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onTest() {
+    return true;
+  }
+
+  onTestObject() {
+    return {redBorder: false};
+  }
+
+}
