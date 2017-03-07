1d8114a11a6fac6e289cc55555375acdf14df62b
Romain Blineau <romain.blineau@gmail.com>
2017-03-07 23:11:30 +0100

    header component creation

--- recipeViewer/src/app/app.component.ts
+++ recipeViewer/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rv-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'rv works!';
 }
