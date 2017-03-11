60daef5091051272c0557e5f8f0aaafde0f5b068
Romain Blineau <romain.blineau@gmail.com>
2017-03-11 13:54:37 +0100

    add first version of recipe-item with harcoded recipe

--- recipeViewer/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeViewer/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rv-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  dummyRecipe = new Recipe('Dummy', 'Dummy', 'https://www.w3schools.com/css/trolltunga.jpg');
 
   constructor() { }
 
